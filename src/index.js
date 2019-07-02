fe167038e3f0a4fce8682a2ea41e6ba432c9d3a0
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-07-02 09:36:43 -0500

    localstorages

--- src/index.js
+++ src/index.js
@@ -1,105 +1,142 @@
-// var 
-//     newUser = [],
-//     increase = [],
-//     users = [],
-//     visit = [];
+// en que lugar se esta
+// si es dashboard
+if(document.getElementById('cards') ){
+    getItemsAll() // obtiene cada elemento del array y lo refleja en cada bloque
+    getItemsSum() // obtiene la suma de todos el arrat y lo pasa a un solo bloque de items
 
-//     const btnSend = document.getElementById("btn-registrer");
-//     //var btnSend = document.querySelector('btn-registrer');
-    
-//     btnSend.addEventListener('click', () => {
-
-//     var txtNewUser = document.querySelector('#new-user').value,
-//         txtIncrease = document.querySelector('#increase').value,
-//         txtUsers = document.querySelector('#users').value,
-//         txtVisit = document.querySelector('#visit').value;
-
-//     newUser.push(txtNewUser);
-//     increase.push(txtIncrease);
-//     users.push(txtUsers);
-//     visit.push(txtVisit);
-
-//     localStorage.setItem('input-new-user', JSON.stringify(txtNewUser));
-//     localStorage.setItem('input-increase', JSON.stringify(txtIncrease));
-//     localStorage.setItem('input-users', JSON.stringify(txtUsers));
-//     localStorage.setItem('input-visit', JSON.stringify(txtVisit));
-    
-//     });
-
-var 
-    newUser = [],
-    income = [],
-    users = [],
-    visit = [];
-
-    // if(localStorage.getItem('input-new-user') != null){
-    //     newUser = JSON.parse(localStorage.getItem('input-new-user'));
-    //     income = JSON.parse(localStorage.getItem('input-increase'));
-    //     user = JSON.parse(localStorage.getItem('input-users'));
-    //     visit = JSON.parse(localStorage.getItem('input-visit'));
-    // }
-
-    //const btnSend = document.getElementById("btn-registrer");
+}else{
     var btnSend = document.querySelector('#btn-registrer');
     btnSend.addEventListener('click', setItems);
 
-    function setItems()  {
-       
-    var txtNewUser = document.querySelector('#new-user').value,
-        txtIncome = document.querySelector('#increase').value,
-        txtUsers = document.querySelector('#users').value,
-        txtVisit = document.querySelector('#visit').value;
+}
 
-    newUser.push(txtNewUser);
-    income.push(txtIncome);
-    users.push(txtUsers);
-    visit.push(txtVisit);
 
-    localStorage.setItem('input-new-user', JSON.stringify(txtNewUser));
-    localStorage.setItem('input-increase', JSON.stringify(txtIncome));
-    localStorage.setItem('input-users', JSON.stringify(txtUsers));
-    localStorage.setItem('input-visit', JSON.stringify(txtVisit));
 
-    getItems();
-    };    
-
-getItems();
-function getItems(){
+// Guardamos items en local storage (form)
+function setItems() {
+    // valores del form
+    var txtNewUser = document.querySelector('#new-user').value, // valor de cantidad compra
+        txtIncome = document.querySelector('#increase').value, //valor de incremento
+        txtUsers = document.querySelector('#users').value,//valor de cantidad usuario
+        txtVisit = document.querySelector('#visit').value;//valor de cantidad visitas
     
-    var cardDiv = document.querySelector('#cards div');
+    // valor de cantidad
+    let backNewUser = window.localStorage.getItem('input-new-user'); // obtenemos lo que ya existe
+    backNewUser = backNewUser ? backNewUser.split(',') : []; // si existe separmos por coma, sino, no hacemos nada
+    backNewUser.push(txtNewUser); // agregamos el nuevo elemento
+    window.localStorage.setItem('input-new-user', backNewUser); // set in local storage
    
-
-    var newUser = JSON.parse(localStorage.getItem('input-new-user')),
-        income = JSON.parse(localStorage.getItem('input-increase')),
-        users = JSON.parse(localStorage.getItem('input-users')),
-        visit = JSON.parse(localStorage.getItem('input-visit'));
     
-    var information = newUser.length;
+    let backtxtIncome = window.localStorage.getItem('input-increase');
+    backtxtIncome = backtxtIncome ? backtxtIncome.split(',') : [];
+    backtxtIncome.push(txtIncome);
+    window.localStorage.setItem('input-increase', backtxtIncome);
 
-        for(var i = 0; i < information; i++){
-            var card = document.createElement('div');
+    let backtxtUsers = window.localStorage.getItem('input-users');
+    backtxtUsers = backtxtUsers ? backtxtUsers.split(',') : [];
+    backtxtUsers.push(txtUsers);
+    window.localStorage.setItem('input-users', backtxtUsers);
 
-            var cardNewUser = document.createElement('div'),
-                cardIncome = document.createElement('div'),
-                cardUsers = document.createElement('div'),
-                cardVisit = document.createElement('div');
+    let backtxtVisit = window.localStorage.getItem('input-visit');
+    backtxtVisit = backtxtVisit ? backtxtVisit.split(',') : [];
+    backtxtVisit.push(txtVisit);
+    window.localStorage.setItem('input-visit', backtxtVisit);
+};
+// Obtenemos el total de cada categoria
+function getItemsSum(){
+    let cantidad = JSON.parse("[" + window.localStorage.getItem('input-new-user') + "]");
+    let incremento = JSON.parse("[" + window.localStorage.getItem('input-increase') + "]");
+    let usuarios = JSON.parse("[" +window.localStorage.getItem('input-users')+ "]");
+    let visitas = JSON.parse("[" +window.localStorage.getItem('input-visit')+ "]");
+    //var newArr = JSON.parse("[" + nuevos + "]");
+    let totalCantidad = 0;
+    let totalincremento = 0;
+    let totalusuarios = 0;
+    let totalvisitas = 0;
+    
+    cantidad.forEach(function(element, index) {
+        //console.log(index,element)
+        totalCantidad  = totalCantidad + parseInt(cantidad[index])
+        totalincremento = totalincremento + parseInt(incremento[index])
+        totalusuarios = totalusuarios + parseInt(usuarios[index])
+        totalvisitas = totalvisitas + parseInt(visitas[index])
+    }); 
 
-            var nodoTextNewUser = document.createTextNode(newUser[i]),
-                nodoTextIncome = document.createTextNode(income[i]),
-                nodoTextUsers = document.createTextNode(users[i]),
-                nodoTextVisit = document.createTextNode(visit[i]);
+    // template
+    var newItems = ` 
+    <div class="rowListCard"> 
+        <div class="card__item">
+            <div class="card__inner">
+            <p class="card-text"><strong>`+totalCantidad+`</strong></p>
+            <h5 class="card-title">Cantidad de nuevas compras</h5>
+            </div>
+        </div>
+        <div class="card__item">
+            <div class="card__inner">
+            <p class="card-text"><strong>`+totalincremento+`%</strong></p>
+            <h5 class="card-title">Incremento de compras</h5>
+            </div>
+        </div>
+        <div class="card__item">
+            <div class="card__inner">
+            <p class="card-text"><strong>`+totalusuarios+`</strong></p>
+            <h5 class="card-title">Cantidad de nuevo usuarios</h5>
+            </div>
+        </div>
+        <div class="card__item">
+            <div class="card__inner">
+            <p class="card-text"><strong>`+totalvisitas+`</strong></p>
+            <h5 class="card-title">Cantidad nuevas visitas</h5>
+            </div>
+        </div>
+    </div>
+    `;
+    document.getElementById("cards").insertAdjacentHTML('afterbegin',newItems);
+}
+// Obtenemos todos elementos como una bitacora
+function getItemsAll(){
+    let cantidad = JSON.parse("[" + window.localStorage.getItem('input-new-user') + "]");
+    let incremento = JSON.parse("[" + window.localStorage.getItem('input-increase') + "]");
+    let usuarios = JSON.parse("[" +window.localStorage.getItem('input-users')+ "]");
+    let visitas = JSON.parse("[" +window.localStorage.getItem('input-visit')+ "]");
+    cantidad.forEach(function(element, index) {
+        //console.log(index,element)
+        let cantidadItem = cantidad[index]
+        let incrementoitem= incremento[index]
+        let usuariositem= usuarios[index]
+        let visitasitem= visitas[index]
 
-                cardNewUser.appendChild(nodoTextNewUser);
-                cardIncome.appendChild(nodoTextIncome);
-                cardUsers.appendChild(nodoTextUsers);
-                cardVisit.appendChild(nodoTextVisit);
 
-                card.appendChild(cardNewUser);
-                card.appendChild(cardIncome);
-                card.appendChild(cardUsers);
-                card.appendChild(cardVisit);
+        var newItems = ` 
+        <div class="rowListCard historico"> 
+            <div class="card__item">
+                <div class="card__inner">
+                <p class="card-text"><strong>`+cantidadItem+`</strong></p>
+                <h5 class="card-title">Cantidad de nuevas compras</h5>
+                </div>
+            </div>
+            <div class="card__item">
+                <div class="card__inner">
+                <p class="card-text"><strong>`+incrementoitem+`%</strong></p>
+                <h5 class="card-title">Incremento de compras</h5>
+                </div>
+            </div>
+            <div class="card__item">
+                <div class="card__inner">
+                <p class="card-text"><strong>`+usuariositem+`</strong></p>
+                <h5 class="card-title">Cantidad de nuevo usuarios</h5>
+                </div>
+            </div>
+            <div class="card__item">
+                <div class="card__inner">
+                <p class="card-text"><strong>`+visitasitem+`</strong></p>
+                <h5 class="card-title">Cantidad nuevas visitas</h5>
+                </div>
+            </div>
+        </div>
+        `;
 
-                cardDiv.appendChild(card);
-        }
-    };
-  
+        var listCard  = document.getElementById("cards").insertAdjacentHTML('afterbegin',newItems);
+
+    }); 
+}
